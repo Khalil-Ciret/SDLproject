48346368625c64418452d3a7045af11d17ae317f
Khalil.ciret <khalil.ciret@gmail.com>
2019-11-14 16:37:24 +0100

    added alpha header and stuff

--- Source.cpp
+++ Source.cpp
@@ -40,6 +40,10 @@ public:
 
 	void setColor(Uint8 red, Uint8 green, Uint8 blue);
 
+	void setBlendMode(SDL_BlendMode blending);
+
+	void setAlpha(Uint8 alpha);
+
 	void render(int x, int y, SDL_Rect* clip = NULL);
 
 	int getWidth();
